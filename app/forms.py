328867e6ad815c3beeb866a4ecda7801c7876785
vps <che-email@yandex.ru>
2017-02-11 09:44:24 +0300

    fix email

--- app/forms.py
+++ app/forms.py
@@ -10,6 +10,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
+from app.models import BasicData
 
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
@@ -30,6 +31,7 @@ class ContactForm(forms.Form):
     email = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea,required=False)
 
+    exclude_bootstrap=[]
     def send_email(self):
         # send email using the self.cleaned_data dictionary
         plaintext = get_template('app/mail_detail.html')
@@ -38,13 +40,21 @@ class ContactForm(forms.Form):
             'email':self.cleaned_data['email'],
             'text':self.cleaned_data['message']}
         message_body = plaintext.render(b)
+        basicdata= BasicData.objects.get(pk=1)
         email = EmailMultiAlternatives('Письмо с сайта Столовой',
                             message_body,
                             #to=['che-email@yandex.ru'
-                            to=['tanuffko24@gmail.com',
+                            to=[basicdata.email,
                                 #'pit-line@bk.ru',
                                 ])
 
         email.attach_alternative(message_body, "text/html")
         email.send()
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if not field in self.exclude_bootstrap:
+                self.fields[field].widget.attrs.update({'class' : 'form-control'})
+
+
 
--- app/models.py
+++ app/models.py
@@ -12,9 +12,10 @@ class BasicData(models.Model):
     tel = models.TextField(verbose_name=u'телефон')
     main_descr = models.TextField(verbose_name=u'главный текст')
     contact_text = models.TextField(verbose_name=u'текст для напишите нам')
-    
+    email = models.EmailField(blank=True)
+
     def __str__(self):
-        return self.canteen_name
+        return self.canteen_name.encode('utf-8')
     class Meta:
         verbose_name=u"общие данные"
         verbose_name_plural=u"общие данные"
